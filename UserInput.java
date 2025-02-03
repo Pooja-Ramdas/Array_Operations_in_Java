a7404a2a8180cddda22785e0f5f1ad121765cece
Pooja Ramdas <pooja.ramdas05@gmail.com>
2025-02-03 22:31:10 +0530

    Added arrayInput function

---
+++ UserInput.java
@@ -0,0 +1,14 @@
+//UserInput.java
+import java.util.*;
+public class UserInput{
+    int[] arrayInput(){
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        System.out.println("Enter 5 numbers: ");
+        for(int i = 0; i < arr.length; i++)
+        {
+            arr[i]= sc.nextInt();
+        }
+        return(arr);
+    }
+}
